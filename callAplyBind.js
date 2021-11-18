6730a102e19f2eee80935545e885b6557ce83d4f
tariqul12 <mshimul591@gmail.com>
2021-11-18 10:47:47 +0600

    call and apply

--- callAplyBind.js
+++ callAplyBind.js
@@ -5,9 +5,9 @@ const normalName = {
     getFullName : function(){
         console.log(this.firstName,this.lastName);
     },
-    chargeBill: function(amount){
+    chargeBill: function(amount,tips,tax){
         console.log(this);
-        this.salary = this.salary-amount;
+        this.salary = this.salary-amount-tips-tax;
         return this.salary;
     }
 
@@ -25,12 +25,28 @@ const friendlyPerson = {
     lastName:"Kalam",
     salary:30000,
 }
-const heroChargeBill=normalName.chargeBill.bind(heroPerson);
-heroChargeBill(2000);
-heroChargeBill(3000);
+// const heroChargeBill=normalName.chargeBill.bind(heroPerson);
+// heroChargeBill(2000);
+// heroChargeBill(3000);
+// console.log(heroPerson.salary);
+// console.log(normalName.salary);
+
+// const friendChargeBill =normalName.chargeBill.bind(friendlyPerson);
+// friendChargeBill(3000);
+// console.log(friendlyPerson.salary);
+
+
+//            call
+normalName.chargeBill.call(heroPerson,9000,100,10);
+// normalName.chargeBill.call(heroPerson,300,30);
 console.log(heroPerson.salary);
+normalName.chargeBill.call(friendlyPerson,5000,300,30);
+console.log(friendlyPerson.salary);
 console.log(normalName.salary);
 
-const friendChargeBill =normalName.chargeBill.bind(friendlyPerson);
-friendChargeBill(3000);
-console.log(friendlyPerson.salary);
+
+
+//                  apply
+normalName.chargeBill.apply(heroPerson,[4000,50,40]);
+console.log(heroPerson.salary);
+
